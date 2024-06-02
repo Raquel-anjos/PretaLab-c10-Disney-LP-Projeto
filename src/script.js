846027d1167edc84da58bb80bad0c03ea6b545bb
Raquel Anjos <rmouzinhoanjos@gmail.com>
2024-06-02 16:13:41 -0300

    Funcionalidade só aceitar numeros entre 1 e 100.

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ let tentativas = 0;
 function jogoDeAdivinhacao() {
    const palpiteDigitado = pegarPalpiteDigitado();
    
+   
    if (!palpiteDigitado) {
     alert("Digite um valor válido");
     return;
@@ -13,13 +14,22 @@ function jogoDeAdivinhacao() {
     alert("Parabéns você acertou!")
     reiniciarJogo();
     return;
+    
+   } else if (palpiteDigitado > 100) {
+    alert("Palpite invalido.")
+    limparPalpiteDigitado();
+    limparPalpiteFalho();
+   } else if (palpiteDigitado < 0) {
+    alert("Palpite invalido.")
+    limparPalpiteDigitado();
+    limparPalpiteFalho();
    } else if (palpiteDigitado > numeroAleatorio) {
     tentativas++;
     atualizarFeedback("O número é muito alto. Tente novamente.")
    } else if (palpiteDigitado < numeroAleatorio) {
     tentativas++;
     atualizarFeedback("O número é muito baixo. Tente novamente.")
-   }
+   } 
 
    const novaPontuacao = 100 - (tentativas * 10)
    atualizarPontuacao(novaPontuacao);
